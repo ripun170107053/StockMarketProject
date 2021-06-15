fb0d8b4f68504438d3563f29ebf02f40207235ed
ripunjoym <53742420+ripun170107053@users.noreply.github.com>
2021-06-15 16:24:33 +0530

    15 june

---
+++ MainProject/src/main/java/com/project/ripunjoy/models/UserModel.java
@@ -0,0 +1,72 @@
+package com.project.ripunjoy.models;
+
+import javax.persistence.*;
+
+
+public class UserModel
+{
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    private String password;
+    private boolean userType;
+
+    private String email;
+    private boolean confirmed;
+
+    public UserModel()
+    {
+
+    }
+
+    private UserModel(String username, String password, boolean userType, String email, boolean confirmed) {
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+        this.email = email;
+        this.confirmed = confirmed;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isUserType() {
+        return userType;
+    }
+
+    public void setUserType(boolean userType) {
+        this.userType = userType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+}
---
+++ MainProject/src/main/java/excel/dao/ExcelRepository.java
@@ -0,0 +1,21 @@
+package excel.dao;
+
+import com.project.ripunjoy.entities.companyEntity;
+import com.project.ripunjoy.models.companyModel;
+import excel.entities.stockPriceEntity;
+import ipo.entities.ipoDetailsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import stockExchange.entities.StockExchangeEntity;
+
+import java.util.List;
+
+@Repository
+public interface ExcelRepository extends JpaRepository<stockPriceEntity,Long>
+{
+
+
+
+}
