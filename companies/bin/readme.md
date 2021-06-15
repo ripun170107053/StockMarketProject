fd6cb546fbd8d0af3e320e8e5966d1a04edb56da
ripunjoym <53742420+ripun170107053@users.noreply.github.com>
2021-06-15 16:20:53 +0530

    Delete companies directory

--- companies/bin/readme.md
+++
@@ -1,2 +0,0 @@
-homecontroller for company is running on port 8365
-sql is running on port 3306
--- companies/bin/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-
-alter table company modify column id int NOT NULL AUTO_INCREMENT;
-insert into company (company_name,turnover,ceo,stock_exchange,board_director,sector,writeup,company_code)
-values('Apple',2021,'TCook','NASDAQ','Stevew',21,'first post',1337);
