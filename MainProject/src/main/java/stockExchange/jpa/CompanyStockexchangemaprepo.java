9deb8c5e3bc685a7424e3a029695c6440840d234
ripunjoym <53742420+ripun170107053@users.noreply.github.com>
2021-06-21 11:06:39 +0530

    Delete MainProject directory

--- MainProject/src/main/java/stockExchange/jpa/CompanyStockexchangemaprepo.java
+++
@@ -1,7 +0,0 @@
-package stockExchange.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import stockExchange.entities.companyStockExchangeMap;
-
-public interface CompanyStockexchangemaprepo extends JpaRepository<companyStockExchangeMap,Long> {
-}
